9a69767a06716077d1e45b947323979b6655309b
Рavel <llianellpp.pp@gmail.com>
2025-05-27 17:40:38 +0300

    upd structure

--- src/components/main.js
+++ src/components/main.js
@@ -1,35 +1,19 @@
 import React from "react";
 
-const Main = () => {
-    return (
-        <main style={ styles.main }>
-            <div style={ styles.container }>
-                <section style={ styles.section }>
-                    <h2>VapeShop</h2>
-                    <p>Test message Test message Test message Test message Test message</p>
-                </section>
-                <section style={ styles.section }>
-                    <h3>Some text Some text Some text</h3>
-                </section>
-            </div>
-        </main>
-    )
-};
+// const Main = () => {
+//     return (
+//         <main style={ styles.main }>
+//             <div style={ styles.container }>
+//                 <section style={ styles.section }>
+//                     <h2>VapeShop</h2>
+//                     <p>Test message Test message Test message Test message Test message</p>
+//                 </section>
+//                 <section style={ styles.section }>
+//                     <h3>Some text Some text Some text</h3>
+//                 </section>
+//             </div>
+//         </main>
+//     )
+// };
 
-const styles = {
-    main: {
-        padding: '20px',
-        minHeight: '400px',
-        flex: 1,
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-
-    section: {
-        marginBottom: '30px',
-        textAlign: 'center',
-    },
-};
-
-export default Main;
+// export default Main;
