a43191e354110017255004de9760341514333ba7
Рavel <llianellpp.pp@gmail.com>
2025-05-29 15:11:16 +0300

    footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
-import "./components/styles/styles.css";
 import Header from "./components/header";
+import Footer from "./components/footer";
 
 function App() {
   return (
     <div style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
       <Header />
+      <Footer />
     </div>
   );
 }
--- src/components/footer.js
+++ src/components/footer.js
@@ -1,9 +1,32 @@
 import React from "react";
+import "./styles/styles-footer.css";
 
-// const Footer = () => {
-//     return (
-        
-//     );
-// };
+const Footer = () => {
+    return (
+        <footer className='footer'>
+            <div className='container'>
+                <section className='sectionLeft'>
+                <div className='catalogSection'>
+                    <p className='legal'>
+                        В соответствии со ст. 20 ФЗ №15 «Об охране здоровья граждан» <br/> лицам, не достигшим 18 лет пользование данным сайтом запрещено.
+                    </p>
+                </div>
+                </section>
+                <section className='sectionRight'>
+                <h3 className='subtitle'>НАШИ КОНТАКТЫ</h3>
+                <ul className='contactsList'>
+                    <li className='contactItem'>88005553535</li>
+                    <li className='contactItem'>10:00 - 20:00</li>
+                    <li className='contactItem'>
+                    <a href="mailto:email@mail.ru" className='link'>
+                        email@mail.ru
+                    </a>
+                    </li>
+                </ul>
+                </section>
+            </div>
+    </footer>
+    );
+};
 
-// export default Footer;
+export default Footer;
